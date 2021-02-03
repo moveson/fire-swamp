3d3bc984af2c9be3c40c440f59279cae39cacc23
Mark Oveson <mark.oveson@mx.com>
2021-02-03 10:09:49 -0700

    Get devise working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+  devise_scope :user do
+    authenticated :user do
+      root "welcome#index", as: :authenticated_root
+    end
+
+    unauthenticated do
+      root "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 end
