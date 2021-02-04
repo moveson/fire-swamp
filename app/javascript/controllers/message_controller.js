ea5327d47ee89cdbebee34afb4ceb65832ce5b1f
Mark Oveson <mark.oveson@mx.com>
2021-02-04 01:15:40 -0700

    Much improved chat app

---
+++ app/javascript/controllers/message_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus'
+import { currentUserId } from '../helpers/auth'
+
+export default class extends Controller {
+    static targets = ['actions']
+    static values = {
+        authorId: String,
+    }
+
+    connect() {
+        if (document.querySelectorAll(`#${this.element.id}`).length > 1) {
+            this.element.remove()
+            return
+        }
+
+        this.element.scrollIntoView({ block: 'nearest' })
+    }
+
+    toggleActions() {
+        if (this.hasActionsTarget && this.authorIdValue === currentUserId()) {
+            this.actionsTarget.classList.toggle('invisible')
+        }
+    }
+}
