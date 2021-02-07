420a0c6bf49fe5f3b7d263adf781c81810540ba9
Mark Oveson <mark.oveson@mx.com>
2021-02-07 15:42:34 -0700

    Remove email validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ require "rails_helper"
 RSpec.describe ::User do
   subject { create(:user) }
 
-  it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_presence_of(:parameterized_username) }
 
